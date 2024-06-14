5f168db9bc6598dc893b5ff6f3012401c66e01bd
Parker Mathis <parkerjmathis@gmail.com>
2024-06-13 18:24:42 -0700

    Updated clicked teams process

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -3,19 +3,21 @@ import { Card, CardBody, Container, Image } from "@chakra-ui/react";
 import questionMarkIconLight from "../assets/question-mark-light.png";
 import questionMarkIconDark from "../assets/question-mark-dark.png";
 
-const GameCard = (team) => {
+const GameCard = ({ team, onSelection }) => {
   const [isClicked, setIsClicked] = useState(false);
 
+  const cardClicked = () => {
+    setIsClicked(true);
+    console.log(team);
+    onSelection(team.abbreviation);
+  };
+
   return (
-    <Card
-      onClick={() => {
-        setIsClicked(true);
-      }}
-    >
+    <Card onClick={cardClicked}>
       <CardBody>
         <Container centerContent>
           {isClicked === true ? (
-            <Image src={team.team.logo} alt="" boxSize="100px" />
+            <Image src={team.logo} alt="" boxSize="100px" />
           ) : (
             <Image src={questionMarkIconDark} alt="" boxSize="100px" />
           )}
--- src/components/GameCardGrid.jsx
+++ src/components/GameCardGrid.jsx
@@ -1,25 +1,33 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import shuffleTeams from "../services/shuffleTeams";
 import reduceTeams from "../services/reduceTeams";
 import TeamMap from "../services/TeamMap";
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-
 const GameCardGrid = () => {
-  let selectedTeams = shuffleTeams(TeamMap);
-  let reducedTeams = reduceTeams(selectedTeams);
-  let duplicateTeams = reducedTeams;
-  let mergeTeams = reducedTeams.concat(duplicateTeams);
-  let finalTeams = shuffleTeams(mergeTeams);
-  console.log(finalTeams);
+  const [finalTeams, setFinalTeams] = useState([]);
+  const [clickedTeams, setClickedTeams] = useState([]);
+  console.log(clickedTeams);
+  const teamClickHandler = (value) => {
+    setClickedTeams([...clickedTeams, value]);
+    console.log(clickedTeams);
+  };
+
+  useEffect(() => {
+    let selectedTeams = shuffleTeams(TeamMap);
+    let reducedTeams = reduceTeams(selectedTeams);
+    let duplicateTeams = reducedTeams;
+    let mergeTeams = reducedTeams.concat(duplicateTeams);
+    let shuffledTeams = shuffleTeams(mergeTeams);
+    setFinalTeams(shuffledTeams);
+  }, []);
 
   return (
     <>
       <SimpleGrid columns={{ sm: 4 }} padding="10px" spacing={6}>
         {finalTeams.map((team, index) => (
-          <GameCard key={index} team={team} />
+          <GameCard key={index} team={team} onSelection={teamClickHandler} />
         ))}
       </SimpleGrid>
     </>
