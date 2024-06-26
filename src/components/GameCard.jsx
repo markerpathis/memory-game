37426cbe5759340f647512299c9534b638750c2b
Parker Mathis <parkerjmathis@gmail.com>
2024-06-25 18:23:28 -0700

    Refactor process for evaluating matches

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,16 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { Card, CardBody, Container, Image } from "@chakra-ui/react";
 import questionMarkIconLight from "../assets/question-mark-light.png";
 import questionMarkIconDark from "../assets/question-mark-dark.png";
 
 const GameCard = ({ team, onSelection }) => {
-  const [isClicked, setIsClicked] = useState(false);
-  const [isCorrect, setIsCorrect] = useState(false);
-
   const cardClicked = () => {
-    setIsClicked(true);
     team.clicked = true;
-    console.log(team);
     onSelection(team);
   };
 
@@ -18,7 +13,7 @@ const GameCard = ({ team, onSelection }) => {
     <Card onClick={cardClicked}>
       <CardBody>
         <Container centerContent>
-          {isCorrect || team.clicked ? (
+          {team.clicked ? (
             <Image src={team.logo} alt="" boxSize="100px" />
           ) : (
             <Image src={questionMarkIconDark} alt="" boxSize="100px" />
--- src/components/GameCardGrid.jsx
+++ src/components/GameCardGrid.jsx
@@ -25,20 +25,20 @@ const GameCardGrid = () => {
   };
 
   if (clickedTeams.length > 1) {
+    let index1 = clickedTeams[0].position;
+    let index2 = clickedTeams[1].position;
     if (clickedTeams[0].abbreviation === clickedTeams[1].abbreviation) {
-      for (let i = 0; i < 2; i++) {
-        let index = clickedTeams[i].position;
-        finalTeams[index].correct = true;
-        setClickedTeams([]);
-      }
-      console.log(finalTeams);
+      setTimeout(() => {
+        finalTeams[index1].clicked = true;
+        finalTeams[index2].clicked = true;
+        setClickedTeams((clickedTeams) => clickedTeams.splice(0, 2));
+      }, 150);
     } else {
-      for (let i = 0; i < 2; i++) {
-        let index = clickedTeams[i].position;
-        finalTeams[index].clicked = false;
-        setClickedTeams([]);
-      }
-      setClickedTeams([]);
+      setTimeout(() => {
+        finalTeams[index1].clicked = false;
+        finalTeams[index2].clicked = false;
+        setClickedTeams((clickedTeams) => clickedTeams.splice(0, 2));
+      }, 150);
     }
   }
 
